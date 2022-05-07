847d0974b933c11ceefb47574bc38fa5e1370fc0
That-Guy07 <103515944+That-Guy07@users.noreply.github.com>
2022-05-07 19:23:31 +0530

    Add files via upload

---
+++ cwh_v6.py
@@ -0,0 +1,4 @@
+# Quiz1-> creat a calculator that can add two numbers taking input from user.
+n1 = input("enter 1st number:- ")
+n2 = input("enter 2nd number:- ")
+print("The sum of two numbers is",int(n1)+int(n2))
